42c37558964ba744f1d1e6da6fde83ee99856a70
SayakSarkar02 <sayaksarkar73@gmail.com>
2024-08-01 04:37:41 +0530

    key rem

--- frontend/app/api/search/route.ts
+++ frontend/app/api/search/route.ts
@@ -14,7 +14,7 @@ export async function POST(req : NextRequest) {
           {
             headers: {
               'Content-Type': 'application/json',
-              'api-key': 'HellYeaH'
+              'api-key': process.env.RK_API_KEY
             }
           });
           console.log(response.data);
